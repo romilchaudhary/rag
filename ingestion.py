ee40d3abbf98d511472a2d716d2a77d384decd77
romilchaudhary <romil.roar@gmail.com>
2026-02-03 11:28:18 +0530

    feat: added required packages

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from langchain_community.embeddings import OllamaEmbeddings
+
 def main():
     print("Hello from rag!")
 
 
 if __name__ == "__main__":
+    print("Ingestion script started...")
     main()
